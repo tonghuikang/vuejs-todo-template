4efcacfaa110be13256f030fb28822862fce8d75
Glitch (vue-vue-hk) <none>
2019-11-02 07:12:18 +0000

    checkpoint saveable

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -5,12 +5,20 @@ new Vue({
     newEntry: "",
     todos: []
   },
+  watch:{
+    todos: {
+      handler: function(newTodos){
+        sessionStorage.setItem('my-todo-list', JSON.stringify(newTodos));
+      },
+      deep: true
+    }
+  },
   methods: {
     addEntry: function(){
       if(this.newEntry){  // if the newEntry is truthy
         this.todos.push({value: this.newEntry, isDone: false});  // add to array
         this.newEntry = "";
-      }      
+      }
     },
     removeEntry: function(index){
       this.todos.splice(index,1);
