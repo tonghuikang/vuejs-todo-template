9c3eadf68096aad8ecef7378edcf8aff9c444f63
Glitch (vue-example) <none>
2019-11-02 14:21:00 +0000

    beautify code

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -5,43 +5,45 @@ new Vue({
     newEntry: "",
     todos: []
   },
-  computed: {  // computed anytime the Vue model is changed
+  computed: {
+    // computed anytime the Vue model is changed
     unfinishedEntries: function() {
-      return this.todos.filter(entry => !entry.isDone); 
+      return this.todos.filter(entry => !entry.isDone);
     }
   },
   watch: {
     todos: {
-      handler: function(newTodos){
-        sessionStorage.setItem('my-todo-list', JSON.stringify(newTodos));
+      handler: function(newTodos) {
+        sessionStorage.setItem("my-todo-list", JSON.stringify(newTodos));
       },
-      deep: true  // to watch all the checkboxes as well
+      deep: true // to watch all the checkboxes as well
     }
   },
   methods: {
-    addEntry: function(){
-      if(this.newEntry){  // if the newEntry is truthy
-        this.todos.push({value: this.newEntry, isDone: false});  // add to array
+    addEntry: function() {
+      if (this.newEntry) {
+        // if the newEntry is truthy
+        this.todos.push({ value: this.newEntry, isDone: false }); // add to array
         this.newEntry = "";
       }
     },
-    removeEntry: function(index){
-      this.todos.splice(index,1);
+    removeEntry: function(index) {
+      this.todos.splice(index, 1);
     }
   },
   // lifecycle hooks
-  beforeCreate: function(){
-    console.log("beforeCreate")
+  beforeCreate: function() {
+    console.log("beforeCreate");
   },
-  created: function(){
-    console.log("created")
+  created: function() {
+    console.log("created");
   },
-  beforeMount: function(){
-    console.log("beforeMount")
+  beforeMount: function() {
+    console.log("beforeMount");
   },
-  mounted: function(){
-    console.log("mounted")
-    const ssTodos = sessionStorage.getItem('my-todo-list');
-    this.todos = ssTodos ? JSON.parse(ssTodos) : [];  // if ssTodos is truthy, parse
+  mounted: function() {
+    console.log("mounted");
+    const ssTodos = sessionStorage.getItem("my-todo-list");
+    this.todos = ssTodos ? JSON.parse(ssTodos) : []; // if ssTodos is truthy, parse
   }
-})
+});
